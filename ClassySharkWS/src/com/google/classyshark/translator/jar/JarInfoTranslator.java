79fc212dad6f1dbb5c5bc1ed533547bfc9856165
Boris Farber <bfarber@google.com>
2015-11-30 11:43:06 +0000

    cosmetic fix

--- ClassySharkWS/src/com/google/classyshark/translator/jar/JarInfoTranslator.java
+++ ClassySharkWS/src/com/google/classyshark/translator/jar/JarInfoTranslator.java
@@ -65,7 +65,7 @@ public class JarInfoTranslator implements Translator {
 
     private static String readableFileSize(long size) {
         if(size <= 0) return "0";
-        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
         int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
         return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
