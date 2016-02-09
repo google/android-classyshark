d21c2409a9007e4117ddafc69f22006a1ee950e4
Boris Farber <bfarber@google.com>
2016-02-09 13:22:25 +0000

    added version 5.5

--- ClassySharkWS/src/com/google/classyshark/gui/panel/displayarea/doodles/SharkBG.java
+++ ClassySharkWS/src/com/google/classyshark/gui/panel/displayarea/doodles/SharkBG.java
@@ -46,5 +46,5 @@ class SharkBG {
                       + "               ii!                             '*YMWM,                        \n"
                       + "               I'                                  \"YM\n"
                       + "\n\n\n\thttp://www.retrojunkie.com/asciiart/animals/sharks.htm"
-                      + "\n\n\n\tClassyShark ver. 5.4 powered by SilverGhost";
+                      + "\n\n\n\tClassyShark ver. 5.5 powered by SilverGhost";
 }
