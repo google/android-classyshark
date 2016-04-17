f0eb6a1b866bedc353d4d52f2d93419c9bdfc676
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-04-17 18:51:10 +0200

    Adds ThemeManager interface
    to start supporting the different themes of the UI

---
+++ ClassySharkWS/src/com/google/classyshark/gui/theme/ThemeManager.java
@@ -0,0 +1,25 @@
+package com.google.classyshark.gui.theme;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This class is the one defining the different parameters needed in order to obtain a proper theme across the whole app
+ */
+public interface ThemeManager {
+
+    ImageIcon getToggleIcon();
+    ImageIcon getRecentIcon();
+    ImageIcon getBackIcon();
+    ImageIcon getForwardIcon();
+    ImageIcon getOpenIcon();
+    ImageIcon getExportIcon();
+    ImageIcon getMappingIcon();
+
+    Color getDefaultColor();
+    Color getKeyWordsColor();
+    Color getIdentifiersColor();
+    Color getAnnotationsColor();
+    Color getSelectionBgColor();
+    Color getNamesColor();
+}
