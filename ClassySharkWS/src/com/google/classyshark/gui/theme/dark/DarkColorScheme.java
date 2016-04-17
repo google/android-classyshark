258b724d41cf4f55779d15fa50d3f5da2072b7e7
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-04-17 22:37:27 +0200

    Adds dark colors and icons

---
+++ ClassySharkWS/src/com/google/classyshark/gui/theme/dark/DarkColorScheme.java
@@ -0,0 +1,16 @@
+package com.google.classyshark.gui.theme.dark;
+
+import java.awt.*;
+
+class DarkColorScheme {
+
+    private DarkColorScheme() {}
+
+    static final Color BACKGROUND = new Color(0,43,54);
+    static final Color DEFAULT = new Color(131, 148, 150);
+    static final Color KEYWORDS = new Color(181, 137, 0);
+    static final Color IDENTIFIERS = new Color(133, 153, 0);
+    static final Color ANNOTATIONS = new Color(108, 113, 196);
+    static final Color SELECTION_BG = new Color(7, 56, 66);
+    static final Color NAMES = new Color(88, 110, 117);
+}
