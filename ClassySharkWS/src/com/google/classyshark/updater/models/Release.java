c855cc83325040c07fbd9075f173ae1628594789
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-04-28 17:07:32 +0200

    Updates the model

--- ClassySharkWS/src/com/google/classyshark/updater/models/Release.java
+++ ClassySharkWS/src/com/google/classyshark/updater/models/Release.java
@@ -1,17 +1,25 @@
 package com.google.classyshark.updater.models;
 
+
+import com.sun.istack.internal.Nullable;
+
 public class Release {
 
-    private final String tag_name;
+    private final String name;
     private final boolean prerelease;
     private final String body;
     private final Assets[] assets;
 
-    public Release(String tag_name, boolean prerelease, String body, Assets[] assets) {
-        this.tag_name = tag_name;
+
+    private Release(String name, boolean prerelease, String body, Assets[] assets) {
+        this.name = name;
         this.prerelease = prerelease;
         this.body = body;
         this.assets = assets;
+    }
+
+    public Release(String name) {
+        this(name, false, "", null);
     }
 
     @Override
@@ -20,16 +28,55 @@ public class Release {
             return false;
         }
 
-        return tag_name.equals(((Release) other).tag_name);
+        return name.equals(((Release) other).name);
+    }
+
+    private int getMajorVersion() {
+        return getVersionField(0);
+    }
+
+    private int getVersionField(int field) {
+        String cleanedReleaseName = name.split("\\s")[0];
+        return Integer.parseInt(cleanedReleaseName.split("\\.")[field]);
+    }
+
+    private int getMinorVersion() {
+        return getVersionField(1);
     }
 
     @Override
     public int hashCode() {
-        return tag_name.hashCode();
+        return name.hashCode();
     }
 
     @Override
     public String toString() {
-        return "REL:\t" + tag_name + "\n\tCHANGELOG:\t" + body + "\n\tURL:\t" + assets[0].getURL();
+        return "REL:\t" + name + "\nCHANGELOG:\n" + body;
+    }
+
+    public boolean isNewerThan(Release other) {
+        return getMajorVersion() > other.getMajorVersion() ||
+                getMajorVersion() == other.getMajorVersion() && getMinorVersion() > other.getMinorVersion();
+    }
+
+    @Nullable
+    public String getDownloadURL() {
+        if (assets != null && assets.length > 0) {
+            return assets[0].getURL();
+        } else {
+            return null;
+        }
+    }
+
+    public String getReleaseName() {
+        return name;
+    }
+
+    public boolean isPrerelease() {
+        return prerelease;
+    }
+
+    public String getChangelog() {
+        return body;
     }
 }
