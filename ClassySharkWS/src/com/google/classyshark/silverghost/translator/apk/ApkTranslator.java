f56b9726f203865dd2909b44b74e69532c48e84e
Boris Farber <boris.farber@gmail.com>
2017-04-13 13:31:50 +0300

    refactoring

--- ClassySharkWS/src/com/google/classyshark/silverghost/translator/apk/ApkTranslator.java
+++ ClassySharkWS/src/com/google/classyshark/silverghost/translator/apk/ApkTranslator.java
@@ -19,8 +19,6 @@ package com.google.classyshark.silverghost.translator.apk;
 import com.google.classyshark.silverghost.TokensMapper;
 import com.google.classyshark.silverghost.translator.Translator;
 import com.google.classyshark.silverghost.translator.apk.dashboard.ApkDashboard;
-import com.google.classyshark.silverghost.translator.apk.dashboard.ClassesDexDataEntry;
-import com.google.classyshark.silverghost.translator.apk.dashboard.asciitable.Table;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -30,7 +28,7 @@ import java.util.List;
  * Translator for the apk
  */
 public class ApkTranslator implements Translator {
-    public static final String[] NEW_LINE = {" ", " "};
+
     private File apkFile;
     private ApkDashboard apkDashboard;
 
@@ -60,55 +58,8 @@ public class ApkTranslator implements Translator {
                 TAG.IDENTIFIER);
         elements.add(element);
 
-
-        String[] headers = {"Entry",
-                "Description"
-        };
-
-        List<String[]> data = new LinkedList<>();
-
-        data.add(NEW_LINE);
-
-        for (ClassesDexDataEntry dexEntry : apkDashboard.getAllDexEntries()) {
-            addRow(data, dexEntry.getName(),
-                    String.valueOf(dexEntry.allMethods) + " methods");
-        }
-
-        data.add(NEW_LINE);
-
-        for (String javaDepError : apkDashboard.getJavaDependenciesErrors()) {
-            addRow(data, "Java ", javaDepError);
-        }
-
-        data.add(NEW_LINE);
-
-        for (String systemAction : apkDashboard.getManifestErrors()) {
-            addRow(data, "System Action ", systemAction);
-        }
-
-        data.add(NEW_LINE);
-
-        for (String nativeLib : apkDashboard.getNativeLibNamesSorted()) {
-            if (!apkDashboard.getPrivateLibErrorTag(nativeLib).isEmpty()) {
-                addRow(data, "Native Error ", nativeLib + " " + apkDashboard.getPrivateLibErrorTag(nativeLib));
-            }
-        }
-
-        String[][] array = new String[data.size()][];
-        for (int i = 0; i < data.size(); i++) {
-            array[i] = data.get(i);
-        }
-
-        element = new ELEMENT(Table.getTable(headers, array).toString(), TAG.DOCUMENT);
+        element = new ELEMENT(apkDashboard.toString(), TAG.DOCUMENT);
         elements.add(element);
-    }
-
-    private void addRow(List<String[]> data, String param1, String param2) {
-        List<String> row = new LinkedList<>();
-        row.add(param1);
-        row.add(param2);
-
-        data.add(row.toArray(new String[0]));
     }
 
     @Override
